1799c351cab973a2e41c319795809f012e9e20e3
a1essar <a1essar@yandex.ru>
2014-03-06 11:07:02 -0800

    Update Bootstrap.php

--- lib/Happy/Bootstrap.php
+++ lib/Happy/Bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Happy CMS 2
+ *
+ * Authors: Isa Ugurchiev, Magamed Esmurziev http://happycms.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Happy;
 
 class Bootstrap
