aa7315d626b0900d6d1c75e2948afc9e358a5268
root <root@ovz12.fastvps.ru>
2014-04-17 19:46:51 +0400

    Added new models

--- lib/Happy/Extensions/Feedback/Controller.php
+++ lib/Happy/Extensions/Feedback/Controller.php
@@ -18,24 +18,67 @@ class Controller extends BaseController
     public function __construct()
     {   
         $this->setController('feedback', function(){
-            $response = $this->feedback();
+            $response = (new FeedbackModel())->feedbackList();
             
             return $this->render([
-                    'template' => 'feedback',
+                    'template' => 'feedback_list',
                     'response' => $response
                 ]
             );          
         });
+        
+        $this->setController('feedback/{id:int}', function($parameters){
+            $response = (new FeedbackModel())->feedbackItem($parameters['id']);
+   
+            if(!empty($response)){
+                return $this->render([
+                        'template' => 'feedback_item',
+                        'response' => $response
+                    ]
+                );   
+            }else{
+                return $this->render([
+                        'template' => '404'
+                    ]
+                );
+            }            
+        });
+        
+        $this->setController('feedback/add', function($parameters){
+            (new FeedbackModel())->feedbackAdd($parameters);            
+        }, 'ajax');
+        
+        $this->setController('@/feedback', function(){
+            $response = (new FeedbackModel())->feedbackList();
+            
+            return $this->render([
+                    'template' => 'admin/feedback_list',
+                    'response' => $response
+                ]
+            );      
+        });
+        
+        $this->setController('@/feedback/{id:int}', function($parameters){
+            $response = (new FeedbackModel())->feedbackItem($parameters['id']);
+            
+            if(!empty($response)){
+                return $this->render([
+                        'template' => 'admin/feedback_item',
+                        'response' => $response
+                    ]
+                );   
+            }else{
+                echo '404';
+            }            
+        });
+        
+        $this->setController('@/feedback/delete', function($parameters){
+            (new FeedbackModel())->feedbackDelete($parameters['id']);          
+        }, 'ajax');
+        
+        $this->setController('@/feedback/update', function($parameters){
+            (new FeedbackModel())->feedbackUpdate($parameters);             
+        }, 'ajax');
     }
-    
-    
-    /** 
-     * Описание метода
-	 */
-	protected function feedback()
-	{
-        $model_feedback = new FeedbackModel();
-        return $model_feedback->feedback();         
-	}
 }
 ?>
